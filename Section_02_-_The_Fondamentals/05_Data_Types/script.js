72a725d6516839bfece2fa760f7d75b1e026202c
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-10 19:24:20 +0200

    Practicing JavaScript data types

---
+++ Section_02_-_The_Fondamentals/05_Data_Types/script.js
@@ -0,0 +1,23 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+let year;
+console.log(typeof year);
+console.log(year);
+
+year = 1991;
+console.log(typeof year);
+console.log(year);
+
+let someIdentifier = null;
+console.log(typeof someIdentifier); // object
+console.log(someIdentifier);
