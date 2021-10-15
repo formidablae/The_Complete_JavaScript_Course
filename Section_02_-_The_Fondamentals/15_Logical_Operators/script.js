8bea9a340638571920f04286e81dd32b9e5b5f66
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-15 22:40:35 +0200

    Practicing Logical operators in The Complete JavaScript Course

---
+++ Section_02_-_The_Fondamentals/15_Logical_Operators/script.js
@@ -0,0 +1,20 @@
+// Boolean logic
+
+const age = 16;
+
+const a = age >= 20;
+const b = age < 30;
+
+// NOT operator
+
+console.log(!a); // true
+
+// AND operator
+
+console.log(a && b); // false
+console.log(!a && b); // true
+
+// OR operator
+
+console.log(a || b); // true
+console.log(a || !b); // false
