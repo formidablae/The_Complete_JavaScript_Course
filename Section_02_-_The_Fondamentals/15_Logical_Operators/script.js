97bf0c49597568f0306aa27bcb5057feca4c7d30
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-15 23:00:14 +0200

    More practicing of Logical operators in The Complete JavaScript Course

--- Section_02_-_The_Fondamentals/15_Logical_Operators/script.js
+++ Section_02_-_The_Fondamentals/15_Logical_Operators/script.js
@@ -18,3 +18,33 @@ console.log(!a && b); // true
 
 console.log(a || b); // true
 console.log(a || !b); // false
+
+const hasDriversLicence = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+const shouldDrive = hasDriversLicence && hasGoodVision;
+
+if (shouldDrive) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
+
+if (hasDriversLicence && hasGoodVision) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
+
+const isTired = true; // C
+console.log(hasDriversLicence && hasGoodVision && isTired);
+
+if (hasDriversLicence && hasGoodVision && !isTired) {
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
