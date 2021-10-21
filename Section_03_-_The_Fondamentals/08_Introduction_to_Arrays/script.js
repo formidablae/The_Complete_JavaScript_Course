ab094fdfbad7a40cd20e20becf5a27645aaae514
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-22 00:12:35 +0200

    Finishing introduction to arrays

--- Section_03_-_The_Fondamentals/08_Introduction_to_Arrays/script.js
+++ Section_03_-_The_Fondamentals/08_Introduction_to_Arrays/script.js
@@ -22,3 +22,18 @@ const firstName = 'Jonas';
 const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
 console.log(jonas);
 console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
