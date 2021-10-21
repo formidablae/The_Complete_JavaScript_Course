41443eeda1a160a6e448802e70990ba2918cc4e6
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-22 00:00:54 +0200

    Doing coding challenge 01 of Section 3 and practicing array in the The Complete JavaScript Course

---
+++ Section_03_-_The_Fondamentals/08_Introduction_to_Arrays/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+// friends = ['Bob', 'Alice']
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
