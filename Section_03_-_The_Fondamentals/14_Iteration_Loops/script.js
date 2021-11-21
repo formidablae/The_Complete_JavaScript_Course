71c35c698de86d84d02730db53ae6261118c8cf8
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-22 00:48:33 +0100

    Watched and coded Iteration: The while loop

--- Section_03_-_The_Fondamentals/14_Iteration_Loops/script.js
+++ Section_03_-_The_Fondamentals/14_Iteration_Loops/script.js
@@ -83,7 +83,27 @@ for (let i = jonasArray.length - 1; i >= 0; i--) {
 for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`-------- Starting exercise ${exercise}`);
     for (let rep = 1; rep < 6; rep++) {
-      console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
     }
 }
-  
+
+// The while Loop
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) {
+        console.log('Loop is about to end...');
+    }
+}
