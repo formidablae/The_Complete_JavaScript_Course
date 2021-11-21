18f50fc70f0dce51324e4a3dfbe128d00d88fff6
formidablae <81068781+formidablae@users.noreply.github.com>
2021-11-22 00:40:29 +0100

    Watched and coded Iteration: The for loop

---
+++ Section_03_-_The_Fondamentals/14_Iteration_Loops/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Iteration: The for Loop
+console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 2');
+console.log('Lifting weights repetition 2');
+console.log('Lifting weights repetition 4');
+console.log('Lifting weights repetition 5');
+console.log('Lifting weights repetition 6');
+console.log('Lifting weights repetition 7');
+console.log('Lifting weights repetition 8');
+console.log('Lifting weights repetition 9');
+console.log('Lifting weights repetition 10');
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights (with for loop) repetition ${rep}`);
+}
+
+for (let rep = 1; rep <= 30; rep++) {
+    console.log(`Lifting weights (with for loop v.2) repetition ${rep}`);
+}
