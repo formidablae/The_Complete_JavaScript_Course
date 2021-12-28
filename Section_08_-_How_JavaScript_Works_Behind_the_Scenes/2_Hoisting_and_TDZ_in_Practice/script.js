60ccba46ce0f18f731f2a0f13fe232d2389cb8db
formidablae <81068781+formidablae@users.noreply.github.com>
2021-12-28 22:43:10 +0100

    Watched and coded Section 08 - How JavaScript Works Behind the Scenes / 2. Hoisting and TDZ in Practice

---
+++ Section_08_-_How_JavaScript_Works_Behind_the_Scenes/2_Hoisting_and_TDZ_in_Practice/script.js
@@ -0,0 +1,47 @@
+'use script';
+
+// Hoisting and TDZ in Practice
+
+// Variables
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
